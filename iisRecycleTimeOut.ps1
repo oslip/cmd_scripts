bfbc7f19bb051671ec57b21338c3dac155f56f4d
oslip <oslip@softserveinc.com>
2019-06-07 15:55:57 +0300

    use variable for name

--- iisRecycleTimeOut.ps1
+++ iisRecycleTimeOut.ps1
@@ -1,6 +1,6 @@
 $srvname = "Name of your Server"
 Invoke-Command -ComputerName $srvrname -ScriptBlock{
-$myApplicationPool = Get-WmiObject -Class IISApplicationPoolSetting -Namespace "root\microsoftiisv2" | Where-Object {$_.Name -eq 'W3SVC/APPPOOLS/AppPool_AdminService'}
+$myApplicationPool = Get-WmiObject -Class IISApplicationPoolSetting -Namespace "root\microsoftiisv2" | Where-Object {$_.Name -eq 'W3SVC/APPPOOLS/$srvname'}
 $myApplicationPool.IdleTimeout=0
 $myApplicationPool.PeriodicRestartSchedule={00:00}
 $myApplicationPool.Put()
